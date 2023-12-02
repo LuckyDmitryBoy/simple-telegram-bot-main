e2f847032032356850eb1f73143dbe83cae1cf21
Dmitry_Lukashevich <lukadmy@gmail.com>
2023-12-02 13:25:25 +0300

    окончательная версия 1

--- src/test/java/io/proj3ct/SpringDemoBot/SpringDemoBotApplicationTests.java
+++ src/test/java/io/proj3ct/SpringDemoBot/SpringDemoBotApplicationTests.java
@@ -2,8 +2,12 @@ package io.proj3ct.SpringDemoBot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("local")
+@TestPropertySource(locations = "classpath:./application.properties")
 class SpringDemoBotApplicationTests {
 
 	@Test
